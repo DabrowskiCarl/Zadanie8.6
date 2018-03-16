762138d30b5da80a6ca0cebb7836f21425176c0f
Karol Dąbrowski <dabrowski.carl@gmail.com>
2018-03-16 20:33:04 +0100

    Add folders, files, contenct, scripts

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var a = 5,
+    b = 2,
+    value = (a * a) - (2 * a * b) - (b * b);
+
+if (value > 0) {
+    console.log('Wartosc dodatnia')
+}
+else if (value < 0) {
+    console.log('Wartosc ujemna')
+} 
+else {
+    console.log('Wynik rowny zero');
+}
